56729837e77fd46c49a9b753b50c2186a5be41e7
romanman <roman@ethergit.com>
2015-08-04 18:26:00 +0300

    Add hash rate check on new blocks

--- src/main/java/com/ethercamp/starter/ethereum/EthereumListener.java
+++ src/main/java/com/ethercamp/starter/ethereum/EthereumListener.java
@@ -4,12 +4,15 @@ import org.ethereum.core.Block;
 import org.ethereum.core.TransactionReceipt;
 import org.ethereum.facade.Ethereum;
 import org.ethereum.listener.EthereumListenerAdapter;
+import org.ethereum.util.BIUtil;
 
+import java.math.BigInteger;
 import java.util.List;
 
 public class EthereumListener extends EthereumListenerAdapter {
 
     Ethereum ethereum;
+    private boolean syncDone = false;
 
     public EthereumListener(Ethereum ethereum) {
         this.ethereum = ethereum;
@@ -19,8 +22,53 @@ public class EthereumListener extends EthereumListenerAdapter {
     public void onBlock(Block block, List<TransactionReceipt> receipts) {
         System.out.println();
         System.out.println("Do something on block: " + block.getNumber());
+
+        if (syncDone)
+            calcNetHashRate(block);
+
         System.out.println();
     }
 
+
+
+    /**
+     *  Mark the fact that you are touching
+     *  the head of the chain
+     */
+    @Override
+    public void onSyncDone() {
+
+        System.out.println(" ** SYNC DONE ** ");
+        syncDone = true;
+    }
+
+    /**
+     * Just small method to estimate total power off all miners on the net
+     * @param block
+     */
+    private void calcNetHashRate(Block block){
+
+        if ( block.getNumber() > 1000){
+
+            long avgTime = 1;
+            long cumTimeDiff = 0;
+            Block currBlock = block;
+            for (int i=0; i < 1000; ++i){
+
+                Block parent = ethereum.getBlockchain().getBlockByHash(currBlock.getParentHash());
+                long diff = currBlock.getTimestamp() - parent.getTimestamp();
+                cumTimeDiff += Math.abs(diff);
+                currBlock = parent;
+            }
+
+            avgTime = cumTimeDiff / 1000;
+
+            BigInteger netHashRate = block.getDifficultyBI().divide(BIUtil.toBI(avgTime));
+            double hashRate = netHashRate.divide(new BigInteger("1000000000")).doubleValue();
+
+            System.out.println("Net hash rate: " + hashRate + " GH/s");
+        }
+
+    }
 
 }
